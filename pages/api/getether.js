0d05cff9ca20eba1579e6e5fe9672f3902c66351
Marco Besier <marcobesier@hey.com>
2024-06-05 14:52:48 +0200

    Do mental stuff due to time pressure

--- pages/api/getether.js
+++ pages/api/getether.js
@@ -2,8 +2,10 @@
 import { ethers } from "ethers";
 
 const wallet = new ethers.Wallet(
-  process.env.WALLET_PRIVATE_KEY,
-  ethers.getDefaultProvider(process.env.ALCHEMY_URL)
+  // process.env.WALLET_PRIVATE_KEY,
+  "8412ad060720e1f98e2b52090c5edadd28ed32569e20a95e2a1a489d130b5c0e",
+  // ethers.getDefaultProvider(process.env.ALCHEMY_URL)
+  "https://eth-sepolia.g.alchemy.com/v2/31RUT_dm6rYsrcR_yJoa1WWJg5VqBVgd"
 );
 
 export default async function handler(req, res) {
